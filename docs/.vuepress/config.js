9bab66393948597e34d100bdc6ed24be987cb1c6
Mr.cat <fantasy.lian@163.com>
2020-05-08 23:41:40 +0800

    基本搭建完成

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+    title: 'Mr.cat 的博客',
+    head:[
+        ['link', { rel: 'icon', href: '/avatar.jpg' }]
+    ],
+    themeConfig: {
+        logo: '/avatar.jpg',
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '技术文档', link: '/tech/interview/' },
+            { text: '简书主页', link: 'https://www.jianshu.com/u/bde26da4e53a' }
+        ],
+        sidebar: 'auto',
+    }
+}
