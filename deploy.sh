4f5bbb01dd36d13f1e5cadca1e3dbe2f429a13e0
Mr.cat <fantasy.lian@163.com>
2020-05-10 15:30:15 +0800

    修改部署配置

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,8 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:nan-gong/FantasyLian.github.io.git master
+git push -f https://github.com/FantasyLian/FantasyLian.github.io.git master
+
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
