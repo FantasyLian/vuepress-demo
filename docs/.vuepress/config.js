463b78cb82865ae8e9bf896d1051d1afa6b1ad6e
Mr.cat <fantasy.lian@163.com>
2021-04-25 15:21:52 +0800

    增加GitHub链接

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,18 @@
-'use strict';
+'use strict'
 
 module.exports = {
-    title: 'Mr.cat 的博客',
-    head:[
-        ['link', { rel: 'icon', href: '/avatar.jpg' }]
+  title: 'Mr.cat 的博客',
+  head: [
+    ['link', { rel: 'icon', href: '/avatar.jpg' }]
+  ],
+  themeConfig: {
+    logo: '/avatar.jpg',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '技术文档', link: '/tech/interview/' },
+      { text: '简书主页', link: 'https://www.jianshu.com/u/bde26da4e53a' },
+      { text: 'GitHub', link: 'https://github.com/FantasyLian' }
     ],
-    themeConfig: {
-        logo: '/avatar.jpg',
-        nav: [
-            { text: '首页', link: '/' },
-            { text: '技术文档', link: '/tech/interview/' },
-            { text: '简书主页', link: 'https://www.jianshu.com/u/bde26da4e53a' }
-        ],
-        sidebar: 'auto',
-    }
+    sidebar: 'auto',
+  }
 }
